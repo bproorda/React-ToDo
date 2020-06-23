0c26933560789d5fb1c9883d9eed3d660808a3dd
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-23 16:32:28 -0500

    added numerical indicator for range slider

--- brenn-todo/src/Components/createToDo/index.js
+++ brenn-todo/src/Components/createToDo/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './createToDo.scss';
 
 export default function Create(props) {
+
+    const [range, setRange] = useState(0);
 
     function SubmitHandler(e) {
         e.preventDefault();
@@ -15,6 +17,11 @@ export default function Create(props) {
         let newHistory = [newHistoryItem, ...props.history];
         console.log(e.target);
         props.addToHistory(newHistory);
+    }
+
+    function rangeHandler(e) {
+        let newRange = e.target.value;
+        setRange(newRange);
     }
 
     return (
@@ -33,8 +40,10 @@ export default function Create(props) {
                         <label name="assignee">Assigned To: </label>
                         <input name="assignee" type="text"></input>
 
-                        <label name="difficulty">Difficulty: </label>
-                        <input  name="difficulty" type="range" min="0" max="5" step="1"></input>
+                        <label name="difficulty">Difficulty: {range !== 0 ? range : 5}</label>
+                        <input onChange={rangeHandler} name="difficulty" type="range" min="0" max="5" step="1"></input>
+
+
                         <button type="submit">Create</button>
                         <button type="reset">Clear Form</button>
                     </fieldset>
