7d7199d0c547943b1aacceff01e85b53afc4fd24
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-22 20:31:34 -0500

    built out login form html/css

--- brenn-todo/src/App.js
+++ brenn-todo/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Header from './Components/header'
 import Footer from './Components/footer'
+import Login from './Components/login'
 import './App.css';
 
 function App() {
   return (
     <>
     <Header/>
-    <div className="App">
+    <div id="App">
         <p>
           Brennan's Todo List: Under Construction!
         </p>
+        <Login/>
     </div>
     <Footer/>
     </>
---
+++ brenn-todo/src/Components/login/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './login.scss'
+
+export default function Login(props){
+
+    return(
+        <>
+        <form>
+            <label>Enter your Name:</label>
+            <input></input>
+            <button type='submit'>Submit</button>
+        </form>
+        </>
+    )
+}
