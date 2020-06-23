ace80f78265a982c64004a6c7e65bbce845c8246
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-22 20:48:41 -0500

    username in header is now a variable passed down from app.js

--- brenn-todo/src/App.js
+++ brenn-todo/src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Components/header'
 import Footer from './Components/footer'
 import Login from './Components/login'
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
     <>
-    <Header/>
+    <Header  userName = {user}/>
     <div id="App">
         <p>
           Brennan's Todo List: Under Construction!
--- brenn-todo/src/Components/header/index.js
+++ brenn-todo/src/Components/header/index.js
@@ -7,14 +7,17 @@ import './header.scss';
 
 export default function Header(props) {
 
-
+    let altName = "Squirrel's"
 
     return (
         <>
-        <header>
-         <img src={logo} id="logo" alt="logo" />
-         <h2>Brenn's To Do List</h2>
-         </header>
+            <header>
+                <img src={logo} id="logo" alt="logo" />
+                <h2>
+                    <b>{ props.userName ? props.userName : altName}</b> 
+                    <p> To Do List</p>
+                   </h2>
+            </header>
         </>
     )
 }
