0610370fac3a53a4ac0f4fd238b1b26ca8f8405f
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-23 16:39:37 -0500

    added classes with terenary statement to toggle background color on todos

--- brenn-todo/src/Components/todos/index.js
+++ brenn-todo/src/Components/todos/index.js
@@ -8,11 +8,16 @@ export default function Todos(props) {
             <div id="todoList">
                 <ul>
                     {props.history.map((todo, index) => (
-                        <li key={index}>
+                        <li key={index} className={todo.completed ? "complete" : "incomplete"}>
                             <h3>To Do: {todo.title}</h3>
                             <p>Assigned to: {todo.assignee}</p>
                             <p>Due Date: {todo.dueDate}</p>
                             <p>Difficulty: {todo.difficulty}</p>
+                            <div className="buttons">
+                                <button>Mark Complete</button>
+                                <button>Edit</button>
+                                <button>Delete</button>
+                            </div>
                         </li>
 
         ))}
