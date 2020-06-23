a94b486cd974d277dc7bb6cab95ced4227d43823
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-23 06:56:36 -0500

    todo from history is not rendering on to todos component

--- brenn-todo/src/Components/todos/index.js
+++ brenn-todo/src/Components/todos/index.js
@@ -5,9 +5,19 @@ export default function Todos(props) {
 
     return (
         <>
-        <div id = "todoList">
-        <p>Where are my To Dos!</p>
-        </div>
+            <div id="todoList">
+                <ul>
+                    {props.history.map((todo, index) => (
+                        <li key={index}>
+                            <h3>To Do: {todo.title}</h3>
+                            <p>Assigned to: {todo.assignee}</p>
+                            <p>Due Date: {todo.dueDate}</p>
+                            <p>Difficulty: {todo.difficulty}</p>
+                        </li>
+
+        ))}
+                </ul>
+            </div>
         </>
     )
 }
