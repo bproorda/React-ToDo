b6db3bfac14f0433aa51c16a9b719fc8d1ec4206
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-24 20:23:46 -0500

    used context to filter out completed todos

--- brenn-todo/src/Components/todos/index.js
+++ brenn-todo/src/Components/todos/index.js
@@ -1,11 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import './todos.scss';
+import useSettings from '../../contexts/settings';
 
 export default function Todos(props) {
     const [completedCount, setCompletedCount] = useState(0);
     const [incompletedCount, setIncompletedCount] = useState(0);
     const {listOfTodos, updateStoredTodos } = props;
+    const {numberPerPage, hideCompleted} = useSettings();
+    const [filteredTodos, setFilteredTodos] = useState( initialFilter());
+    
 
+
+    function initialFilter(){
+        if(!hideCompleted) {
+            //setFilteredTodos(listOfTodos);
+            return listOfTodos
+        } else {
+            let filteredTodos = listOfTodos.filter(todo => !todo.completed );
+            //setFilteredTodos(filteredTodos);
+            return filteredTodos;
+        }
+    }
 
     function toggleComplete(index) {
         let updatedTodos = listOfTodos.map((todo, id) => {
@@ -42,6 +57,17 @@ export default function Todos(props) {
         setIncompletedCount(Icount);
     }, [listOfTodos])
 
+    useEffect(() => {
+        if(!hideCompleted) {
+            setFilteredTodos(listOfTodos);
+            return listOfTodos
+        } else {
+            let filteredTodos = listOfTodos.filter(todo => !todo.completed );
+            setFilteredTodos(filteredTodos);
+            return filteredTodos;
+        }
+    }, [hideCompleted, listOfTodos]);
+
     return (
         <>
             <div id="todoList">
@@ -50,7 +76,7 @@ export default function Todos(props) {
                     <h2>Completed To Dos: <span id="cc">{completedCount}</span></h2>
                 </div>
                 <ul>
-                    {listOfTodos.map((todo, index) => (
+                    {filteredTodos.map((todo, index) => (
                         <li key={todo.id} className={todo.completed ? "complete" : "incomplete"}>
                             <h3>To Do: {todo.title}</h3>
                             <p>Assigned to: {todo.assignedTo}</p>
