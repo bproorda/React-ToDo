4a0e622c7a412961af21c3b75f5dcf910bb94796
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-24 20:00:17 -0500

    filled out settings.js

--- brenn-todo/src/contexts/settings.js
+++ brenn-todo/src/contexts/settings.js
@@ -3,5 +3,24 @@ import React, { useContext } from 'react';
 export const SettingsContext = React.createContext();
 
 export class SettingsProvider extends React.Component{
-    
+    constructor(props) {
+        super(props); 
+
+        this.state = {
+            numberPerPage: 5,
+            hideCompleted: true,
+        }
+    }
+
+    render() {
+        return (
+          <SettingsContext.Provider value={this.state}>
+            {this.props.children}
+          </SettingsContext.Provider>
+        )
+      }
+        
 }
+export default function useSettings() {
+    return useContext(SettingsContext);
+  }
