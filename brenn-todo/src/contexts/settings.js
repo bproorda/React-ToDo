8c03517f6e63f1a2cf68ff312d7e911d8cf75c04
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-24 19:51:28 -0500

    scaffolded out context files

---
+++ brenn-todo/src/contexts/settings.js
@@ -0,0 +1,7 @@
+import React, { useContext } from 'react';
+
+export const SettingsContext = React.createContext();
+
+export class SettingsProvider extends React.Component{
+    
+}
--- brenn-todo/src/index.js
+++ brenn-todo/src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter} from "react-router-dom";
+import { SettingsProvider } from './contexts/settings';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+    <SettingsProvider>
     <App />
+    </SettingsProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
